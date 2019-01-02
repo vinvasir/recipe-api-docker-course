a1bb6b541fb77d1579e259dee8e0f19fc404e7aa
Prateek Vasireddy <prat.vasireddy@gmail.com>
2019-01-02 18:13:02 -0500

    testing and linting

--- app/app/test.py
+++ app/app/test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
+
 
 class CalcTest(TestCase):
     def test_add_numbers(self):
@@ -11,3 +12,7 @@ class CalcTest(TestCase):
         x = 5
         y = 4
         self.assertEqual(9, add(x, y))
+
+    def test_substract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(6, subtract(5, 11))
