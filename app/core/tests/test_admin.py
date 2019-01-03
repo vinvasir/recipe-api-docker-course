2bc4140e8ef131d13bb9ace0ae259a4858cafd51
Prateek Vasireddy <prat.vasireddy@gmail.com>
2019-01-02 21:24:29 -0500

    tdd the admin area

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@londonappdev.com',
+            password='test123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='test123',
+            name='Test Userfullname'
+        )
+
+    def test_users_listed(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
