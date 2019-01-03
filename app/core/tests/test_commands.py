51e79713cb83956687c48dfe4c448fdc98f3e4f8
Prateek Vasireddy <prat.vasireddy@gmail.com>
2019-01-02 22:11:01 -0500

    set up the correct temporary dependency build steps for postgres driver in the Dockerfile

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(1, gi.call_count)
